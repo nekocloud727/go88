eff76d3c38fff97cedf994b2a64bdf990c62b68c
nekocloud727 <nguyenanhtuan12345h@gmail.com>
2025-04-19 13:10:55 +0700

    socket config

--- config/database.js
+++ config/database.js
@@ -1,24 +1,24 @@
-module.exports = {
-	// 'url': 'mongodb://127.0.0.1:27017',
-	'url': 'mongodb://192.168.1.111:27017',
-	'options': {
-		'dbName': 'CLUB3333', // red
-		//'dbName': 'GAME', // red
-		//'dbName': 'admin', // red
-                //'dbName': 'vn11022021', // red
-		'useNewUrlParser': true,
-		'useUnifiedTopology': true,
-		//'autoIndex': false,
-	},
-};
-
 // module.exports = {
-// 	'url': 'mongodb://127.0.0.1:27017',
+// 	// 'url': 'mongodb://127.0.0.1:27017',
+// 	'url': 'mongodb://192.168.1.111:27017',
 // 	'options': {
-// 		'dbName': 'admin', // red
+// 		'dbName': 'CLUB3333', // red
+// 		//'dbName': 'GAME', // red
+// 		//'dbName': 'admin', // red
+//                 //'dbName': 'vn11022021', // red
 // 		'useNewUrlParser': true,
 // 		'useUnifiedTopology': true,
-// 		'autoIndex': false,
+// 		//'autoIndex': false,
 // 	},
 // };
+
+module.exports = {
+	'url': 'mongodb+srv://root:123@test.lxugytw.mongodb.net/?retryWrites=true&w=majority&appName=test',
+	'options': {
+		'dbName': 'test', // red
+		'useNewUrlParser': true,
+		'useUnifiedTopology': true,
+		'autoIndex': false,
+	},
+};
 
