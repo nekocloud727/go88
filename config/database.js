e09ccfb6c58e7019891052fe339970ad4c2e469c
nekocloud727 <nguyenanhtuan12345h@gmail.com>
2025-04-19 12:37:33 +0700

    test

---
+++ config/database.js
@@ -0,0 +1,24 @@
+module.exports = {
+	// 'url': 'mongodb://127.0.0.1:27017',
+	'url': 'mongodb://192.168.1.111:27017',
+	'options': {
+		'dbName': 'CLUB3333', // red
+		//'dbName': 'GAME', // red
+		//'dbName': 'admin', // red
+                //'dbName': 'vn11022021', // red
+		'useNewUrlParser': true,
+		'useUnifiedTopology': true,
+		//'autoIndex': false,
+	},
+};
+
+// module.exports = {
+// 	'url': 'mongodb://127.0.0.1:27017',
+// 	'options': {
+// 		'dbName': 'admin', // red
+// 		'useNewUrlParser': true,
+// 		'useUnifiedTopology': true,
+// 		'autoIndex': false,
+// 	},
+// };
+
